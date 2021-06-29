913d1a256edfab99e700b49f5548709295531671
Fernando04 <fer04.oso@gmail.com>
2021-06-29 17:04:27 -0500

    Store

--- src/main/kotlin/PostWork1.kt
+++ src/main/kotlin/PostWork1.kt
@@ -1,15 +1,18 @@
 
 fun main() {
-        val nombre: String
-        val apellido: String
-        val Direccion: String
-        val email: String
-        val contraseña: String
+        val nombre: String = "Fernando"
+        val apellido: String = "Torres"
+        val direccion: String ="Palermo 56, Izcalli Piramide"
+        val email: String ="info@gmail.com"
         var precioAtun: Int = 15
         var precioArroz: Int = 13
         var precioJabon: Int = 11
         var precioJamon: Float = 25.5f
-
+    println("Hola ${nombre}${apellido}")
+        println("Tu productos seran enviados a: ${direccion}")
+    println("Tu recibo sera enviado a: ${email}")
+    println("Qué productos desea agregar: ")
+        print("Atun, Arroz, Jabón, Jamon \nsu total es: ")
     var totalProductosComprados: Float
     totalProductosComprados = precioJamon + precioArroz + precioAtun + precioJabon
         println("${totalProductosComprados}")
